16c97a19fa4c6c3c42f196cb7a93ecdf626c85a9
Ahmed El-bastawisi <ahmed.elbastawisi@outlook.com>
2020-08-09 20:19:42 +0200

    minor fix

--- backend/models.py
+++ backend/models.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 import json
 
 database_name = "trivia"
-asasdatabase_path = "postgres:///{}".format(database_name)
+database_path = "postgres:///{}".format(database_name)
 
 db = SQLAlchemy()
 
